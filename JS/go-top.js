85c576d24fcba1c73233ae933c26659d1437cf95
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-11-10 17:41:35 -0300

    dolor

---
+++ JS/go-top.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("down", window.scrollY > 0);
+});
+
+window.addEventListener("scroll", function () {
+  let navmenu__menu = document.querySelector(".nav-menu__menu");
+  navmenu__menu.classList.toggle("down", window.scrollY > 0);
+});
+
+window.addEventListener("scroll", () => {
+  const toTop = document.querySelector(".to-top");
+  if (window.scrollY > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
